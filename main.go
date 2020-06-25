1645e0470ba9b840b33caddb6289400486052649
Michael Smith <mike@cadmia.io>
2020-06-25 20:10:20 +0100

    newCard func - typed returns

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	card := "Ace of Spades"   //Variable declaration - alternative is var card = "card" or var card string = "card"
-	card = "Five of Diamonds" //Colon only needed for initialisation - can just assign
-
+	card := "Ace of Spades" //Variable declaration - alternative is var card = "card" or var card string = "card"
+	card = newCard()
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
