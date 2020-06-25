2ae12d77fd51e3f36d278242db232c016d18fb79
Michael Smith <mike@cadmia.io>
2020-06-25 20:08:32 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	card := "Ace of Spades"   //Variable declaration - alternative is var card = "card" or var card string = "card"
+	card = "Five of Diamonds" //Colon only needed for initialisation - can just assign
+
+	fmt.Println(card)
+}
