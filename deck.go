e81199033e0ca744c936e1e6f931b85883de260e
Michael Smith <mike@cadmia.io>
2020-06-26 23:09:50 +0100

    Refactor deck.go - tidy up structure, rename funcs, return errors from methods

--- deck.go
+++ deck.go
@@ -34,11 +34,26 @@ func newDeck() deck {
 	return cards
 }
 
-// Prints all the cards in a deck
-func (d deck) print() {
-	for _, card := range d {
-		fmt.Println(card)
+// Function to create a deck from a file
+func newDeckFromFile(filename string) deck {
+	// ReadFile ioutil method - returns a byte slice and an error
+	b, err := ioutil.ReadFile(filename)
+	// Check the error - panic if error not nil
+	check(err)
+
+	// Check to see if the filename is of type json
+	if r.MatchString(filename) {
+		// Declare an empty variable of type deck
+		var d deck
+		// Unmarshal the json - unmarshal takes a byte slice and a pointer to a variable then typecasts the json
+		// into that type and stores it in the variable - or errors if the type is not suitable
+		err := json.Unmarshal(b, &d)
+		check(err)
+		// return the json decoded deck
+		return d
 	}
+	// If deck is not saved as a JSON assume deck is saved as comma delimited string, split the string into a []string then typecast to a deck
+	return deck(strings.Split(string(b), ","))
 }
 
 // Deals n cards
@@ -47,9 +62,11 @@ func (d deck) deal(handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
-// Utility function to join a deck into a string and then typecast to a byte slice
-func (d deck) toString() string {
-	return strings.Join(d, ",")
+// Prints all the cards in a deck
+func (d deck) print() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
 }
 
 // Utility function to convert a deck in a byte slice JSON
@@ -64,31 +81,13 @@ func (d deck) toJSON() []byte {
 	return b
 }
 
-// Function to read the deck from a file
-func loadDeckFromFile(filename string) deck {
-	// ReadFile ioutil method - returns a byte slice and an error
-	b, err := ioutil.ReadFile(filename)
-	// Check the error - panic if error not nil
-	check(err)
-
-	// Check to see if the filename is of type json
-	if r.MatchString(filename) {
-		// Declare an empty variable of type deck
-		var d deck
-		// Unmarshal the json - unmarshal takes a byte slice and a pointer to a variable then typecasts the json
-		// into that type and stores it in the variable - or errors if the type is not suitable
-		err := json.Unmarshal(b, &d)
-		// Check error and panic if not nil
-		check(err)
-		// return the json decoded deck
-		return d
-	}
-	// If deck is not saved as a JSON assume deck is saved as comma delimited string, split the string into a []string then typecast to a deck
-	return deck(strings.Split(string(b), ","))
+// Utility function to join a deck into a string and then typecast to a byte slice
+func (d deck) toString() string {
+	return strings.Join(d, ",")
 }
 
 // Save a deck to a JSON file
-func (d deck) saveToFile(filename string) {
+func (d deck) saveToFile(filename string) error {
 	var b []byte
 	// Check if file is json using global regex - convert to json if true or byte slice string if false
 	if r.MatchString(filename) {
@@ -97,10 +96,7 @@ func (d deck) saveToFile(filename string) {
 		b = []byte(d.toString())
 	}
 	// Writes the deck to the specified filename in the current dir
-	err := ioutil.WriteFile("./"+filename, b, 0644)
-
-	// Check to see if error returned from WriteFile and panic if error present
-	check(err)
+	return ioutil.WriteFile("./"+filename, b, 0644)
 }
 
 // Shuffle the deck
